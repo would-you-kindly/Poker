ac410ea1917f27f205bd7fd2cf8d317583eb80bc
would-you-kindly <would.you.kindly@yandex.ru>
2018-03-12 17:58:19 +0500

    Убрал все лишние Using'и

--- HoldemServer/Server.cs
+++ HoldemServer/Server.cs
@@ -2,14 +2,11 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Messaging;
 using System.Net;
 using System.Net.Sockets;
 using System.Runtime.Serialization.Formatters.Binary;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace HoldemServer
 {
